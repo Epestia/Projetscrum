dab4e2bfd5dafb5adce0b5bd53608deafed5da38
Giovanni Dylan Menegon <64831113+Epestia@users.noreply.github.com>
2020-06-22 13:57:01 +0200

    Update Program.cs

--- ProjetScrum/ProjetScrum/Program.cs
+++ ProjetScrum/ProjetScrum/Program.cs
@@ -10,7 +10,7 @@ namespace ProjetScrum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Felicien");
+            Console.WriteLine("Hello world");
             Console.ReadKey();
         }
     }
