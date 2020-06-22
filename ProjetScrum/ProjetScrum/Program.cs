bbc720afa635a44114769c2e049988419e33e98a
s_10253_net <s_10253_net@D-W10-LEO09>
2020-06-22 15:18:31 +0200

    modif exo 2

--- ProjetScrum/ProjetScrum/Program.cs
+++ ProjetScrum/ProjetScrum/Program.cs
@@ -4,20 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace ProjetScrum
+namespace ProjetScrum2
 {
-    class Program
+    class Program2
     {
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("Choisir un option:");
-            Console.WriteLine("1) Nouvelle personne");
-            Console.WriteLine("2) supprimer personne");
-            Console.WriteLine("3) Sauver et Quitter");
-            Console.Write("\r\nChoisir un option : ");
-            Console.ReadKey();
-
+            Console.Write("\r\nEncodage films : ");
         }
     }
 }
