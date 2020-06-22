fb8a96d60731f0c56a3287fc6ca42be4e37e56e0
Felicien MANZI <35733583+gitmanfel@users.noreply.github.com>
2020-06-22 13:57:04 +0200

    commit "update"

--- ProjetScrum/ProjetScrum/Program.cs
+++ ProjetScrum/ProjetScrum/Program.cs
@@ -10,8 +10,14 @@ namespace ProjetScrum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.Clear();
+            Console.WriteLine("Choisir un option:");
+            Console.WriteLine("1) Nouvelle personne");
+            Console.WriteLine("2) supprimer personne");
+            Console.WriteLine("3) Sauver et Quitter");
+            Console.Write("\r\nChoisir un option : ");
             Console.ReadKey();
+
         }
     }
 }
